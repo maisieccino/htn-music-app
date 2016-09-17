1a907cfe98a755bdf9294283bd3ed35b547e07cc
Matthew Bell <logicnotlogical@outlook.com>
2016-09-17 16:41:21 -0400

    Add test GET endpoint

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,16 @@ const upload = multer({ dest: './files' });
 var app = express();
 
 app.post('/upload', upload.single('file'), function (req, res) {
-    return req.file.filename;
+    var returnData = {
+        filename: req.file.filename,
+        url: "https://giphy.com/gifs/animal-parrot-rRLAQHL9qHzNK",
+        spiciness: 0.8
+    };
+    return returnData;
+});
+
+app.get('/', function (req, res) {
+    return "Sup.";
 });
 
 app.listen(3001, function () {
