702678ee04e6a3b911cdb3d390dc7673d37ddaa8
Matthew Bell <logicnotlogical@outlook.com>
2016-09-17 16:27:18 -0400

    Add simple uploading

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,14 @@
 "use strict";
 
 const express = require('express');
+const multer = require('multer');
+const upload = multer({ dest: './files' });
 
 var app = express();
+
+app.post('/upload', upload.single('file'), function (req, res) {
+    return req.file.filename;
+});
 
 app.listen(3001, function () {
     console.log('sick');
---
+++ backend/music.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const http = require('http');
+const secrets = require('../secrets);
+
+var processSong = function (song) {
+    
+};
+
+module.exports = {
+    processSong: processSong
+};
