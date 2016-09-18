bf692fbe4d982d73aa6b2124a83cdb9b6b40f57d
Matthew Bell <logicnotlogical@outlook.com>
2016-09-17 22:25:05 -0400

    Upload returns junk data, just for testing

--- backend/index.js
+++ backend/index.js
@@ -9,13 +9,13 @@ const fs = require('fs');
 var app = express();
 
 app.post('/upload', upload.single('file'), function (req, res) {
-    var sth = music.processSong(fs.readFileSync(req.file.path));
+    // var sth = music.processSong(fs.readFileSync(req.file.path));
     var returnData = {
         filename: req.file.filename,
         url: "https://giphy.com/gifs/animal-parrot-rRLAQHL9qHzNK",
         spiciness: 0.8
     };
-    return res.send(sth);
+    return res.send(returnData);
 });
 
 app.get('/', function (req, res) {
