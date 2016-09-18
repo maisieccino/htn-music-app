58f49d25de20c0d6429e8462554c22936189b120
Matthew Bell <logicnotlogical@outlook.com>
2016-09-18 10:29:56 -0400

    Analyse key

--- public/js/music.js
+++ public/js/music.js
@@ -22,6 +22,34 @@ var sonicApiUpload = function(song, next) {
         }, next);
 };
 
+var sonicApiAnalyseKey = function(file_id, next) {
+    var returnData;
+    needle.post('https://api.sonicAPI.com/analyze/tempo',
+        {
+            access_id: secrets.sonicapi,
+            input_file: file_id,
+            format: 'json',
+            blocking: true
+        },
+        {
+            multipart: true
+        }, function (err, res, body) {
+            if (!err) {
+                var key = body.tonart_result.key;
+                switch (1) {
+                    case /min/.match(key): next('minor');
+                        break;
+                    case /maj/.match(key): next("major");
+                        break;
+                    default:
+                        next("major");
+                        break;
+                }
+            }
+            else return err;
+        });
+};
+
 var processSong = function (song, next) {
     sonicApiUpload(song, function (err, res, body) {
         if (!err) {
